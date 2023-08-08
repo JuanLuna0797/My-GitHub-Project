3142f15e7ea5883358a10bf5c8f2d8cbb7b3677d
JMLuna <jmlunaj@pucp.edu.pe>
2023-08-07 20:25:53 -0500

    Se creo el archivo hello.c para la prueba y su posterior compilacion

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Insert a number to print: ");
+    scanf("%d",&num);
+    printf("The inserted number was: %d\n",num);
+
+    return 0;
+}
